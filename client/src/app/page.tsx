560ecaeb5677ce5af63faa732cd3aa0dfc2280b2
sidhyaashu <ashutoshsidhya69@gmail.com>
2024-11-05 01:58:45 +0530

    auth

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -3,10 +3,10 @@ import HeroSection from "@/components/base/HeroSection";
 import FeatureSection from "@/components/base/FeatureSection";
 import UserReviews from "@/components/base/UserReviews";
 import Footer from "@/components/base/Footer";
-import { authOption, CustomSession } from "./api/auth/[...nextauth]/options";
+import { authOptions, CustomSession } from "./api/auth/[...nextauth]/options";
 import { getServerSession } from "next-auth";
 export default async function LandingPage() {
-  const session: CustomSession | null = await getServerSession(authOption);
+  const session: CustomSession | null = await getServerSession(authOptions);
   return (
     <div className="min-h-screen flex flex-col ">
       {/* Header */}
